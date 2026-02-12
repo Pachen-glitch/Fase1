980287dbc9ff16b34920a59b69e4d9ec676600fa
Jorge <jjmarcant06@gmail.com>
2026-02-11 20:21:39 -0600

    INTERFAZ OPCODES

--- src/main/java/com/edu/interpreter/opcodes/Opcode.java
+++ src/main/java/com/edu/interpreter/opcodes/Opcode.java
@@ -1,5 +1,12 @@
 package main.java.com.edu.interpreter.opcodes;
 
+import javax.script.ScriptException;
+
+import main.java.com.edu.interpreter.engine.ExecutionContext;
+
+@FunctionalInterface
 public interface Opcode {
-    //void execute(ExecutionContext ctx);
+
+    void execute(ExecutionContext context) throws ScriptException;
+
 }
