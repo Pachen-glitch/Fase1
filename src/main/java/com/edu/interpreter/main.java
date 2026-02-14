90f4f41ac638cabce46f83ebae718fdef4e84c91
Jorge <jjmarcant06@gmail.com>
2026-02-13 18:45:00 -0600

    Main

--- src/main/java/com/edu/interpreter/main.java
+++ src/main/java/com/edu/interpreter/main.java
@@ -1,25 +1,25 @@
 package main.java.com.edu.interpreter;
 
-import main.java.com.edu.interpreter.opcodes.*;
-import main.java.com.edu.interpreter.engine.ExecutionContext;
+import main.java.com.edu.interpreter.engine.ScriptInterpreter;
+
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        OpcodeRegistry registry = new OpcodeRegistry();
+        ScriptInterpreter interpreter = new ScriptInterpreter();
 
-        ArithmeticOpcodes.register(registry);
-        StackOpcodes.register(registry);
-        LogicOpcodes.register(registry);
-        CryptoOpcodes.register(registry);
+        // Caso PDF
+        boolean result1 = interpreter.execute("1 2 OP_ADD 5 OP_GREATERTHAN");
+        System.out.println("Caso PDF: " + result1);
 
-        ExecutionContext context = new ExecutionContext();
+        // Igualdad 
+        boolean result2 = interpreter.execute("1 1 OP_EQUAL");
+        System.out.println("1 1 OP_EQUAL: " + result2);
 
-        registry.get("OP_2").execute(context);
-        registry.get("OP_2").execute(context);
-        registry.get("OP_EQUAL").execute(context);
-
-        context.getStack().printStack();
-
+        // P2PKH ( no representa un script real de P2PKH)
+        boolean result3 = interpreter.execute(
+            "VALIDA PUBKEY OP_DUP OP_HASH160 HASH_PUBKEY OP_EQUALVERIFY OP_CHECKSIG"
+        );
+        System.out.println("P2PKH: " + result3);
     }
 }
